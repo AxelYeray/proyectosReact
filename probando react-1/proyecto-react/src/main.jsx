5f86f2170d06d3a230079cfad48b059809ff62e6
Axel <axel00326@gmail.com>
2023-07-29 16:51:43 -0600

    react

--- probando react-1/proyecto-react/src/main.jsx
+++ probando react-1/proyecto-react/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { PrimerComponente } from './PrimerComponente'
 import './style.css'
+import { ListadoApp } from './ListadoApp'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimerComponente />
+    <ListadoApp />
   </React.StrictMode>,
 )
